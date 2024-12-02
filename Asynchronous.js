12c02550b141505b631bc07f2fd553055a5e5846
DevaniVedant <vedantdevani0405@gmail.com>
2024-12-02 18:40:49 +0530

    Sync Async Data

---
+++ Asynchronous.js
@@ -0,0 +1,9 @@
+ 
+let x=10;
+let y=0;
+ 
+setTimeout(() => {
+     y = 20;
+ }, 2000);
+ 
+console.log(x+y);
---
+++ Synchronous.js
@@ -0,0 +1,4 @@
+ 
+console.log("Function 1");
+console.log("Function 2");
+console.log("Function 3");
